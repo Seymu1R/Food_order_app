90eccd7331dda5feabec03852549eaa27d244529
Seymu1R <Seymuram@code.edu.az>
2022-12-28 03:56:35 +0400

    sa

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -18,8 +18,8 @@ function HeaderCardButton(props) {
         <CartIcon/>
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge} >99</span>
-        salam
+        <span className={classes.badge} >{numberOfCartItems}</span>
+        
       </button>
   )
 }
